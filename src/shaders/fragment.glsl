1c45225461ae95f5807f941d0a7fd0122055cf9e
X3ne <X3ne@users.noreply.github.com>
2023-12-12 16:45:22 +0100

    feat(shader): add crop feature to the shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,11 +6,13 @@ uniform vec4 resolution;
 uniform vec2 mouse;
 uniform vec2 threshold;
 uniform float pixelRatio;
+uniform bool crop;
 uniform sampler2D image0;
 uniform sampler2D image1;
 
 const float HALF = 0.5;
 const float ONE = 1.0;
+const float BORDER_WIDTH = 0.0001;
 
 vec2 mirrored(vec2 v) {
   vec2 m = mod(v, 2.0);
@@ -27,5 +29,12 @@ void main() {
   vec2 displacement = (depthMapValue.r - HALF) * mouse.xy / threshold;
   vec2 fake3d = vUv + displacement;
 
-  gl_FragColor = texture2D(image0, mirrored(fake3d));
+  if (!crop && (
+    fake3d.x < BORDER_WIDTH || fake3d.x > ONE - BORDER_WIDTH ||
+    fake3d.y < BORDER_WIDTH || fake3d.y > ONE - BORDER_WIDTH
+  )) {
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
+  } else {
+    gl_FragColor = texture2D(image0, mirrored(fake3d));
+  }
 }
