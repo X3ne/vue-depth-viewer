73ea505264743877790240fd54bbca9c334b1477
X3ne <X3ne@users.noreply.github.com>
2023-12-18 18:35:55 +0100

    feat: better typescript implementation for props autocomplete

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import { defineComponent, onBeforeUnmount, onMounted, ref, watch } from 'vue-demi'
-import { props } from './props'
 import { Viewer } from './viewer'
 import './style.css'
 
-export default defineComponent({
+export interface DepthViewerOptions {
+  horizontalThreshold?: number
+  verticalThreshold?: number
+  crop?: boolean
+  useMouse?: boolean
+  useScreen?: boolean
+}
+
+export interface DepthViewerProps {
+  img: string
+  depthImg: string
+  options?: DepthViewerOptions
+}
+
+export default defineComponent<DepthViewerProps>({
   name: 'VueDepthViewer',
-  props,
+  props: ['options'] as unknown as undefined,
   setup(props) {
     const viewerRef = ref<HTMLDivElement | null>(null)
     let viewer: Viewer | null = null
@@ -14,11 +27,11 @@ export default defineComponent({
       viewer = new Viewer(viewerRef, {
         image: props.img,
         depthImage: props.depthImg,
-        horizontalThreshold: props.options.horizontalThreshold,
-        verticalThreshold: props.options.verticalThreshold,
-        crop: props.options.crop,
-        useMouse: props.options.useMouse,
-        useScreen: props.options.useScreen,
+        horizontalThreshold: props.options?.horizontalThreshold,
+        verticalThreshold: props.options?.verticalThreshold,
+        crop: props.options?.crop,
+        useMouse: props.options?.useMouse,
+        useScreen: props.options?.useScreen,
       })
     })
 
@@ -33,18 +46,18 @@ export default defineComponent({
         viewer.rerender({
           image: newProps.img,
           depthImage: newProps.depthImg,
-          horizontalThreshold: newProps.options.horizontalThreshold,
-          verticalThreshold: newProps.options.verticalThreshold,
-          crop: newProps.options.crop,
-          useMouse: props.options.useMouse,
-          useScreen: props.options.useScreen,
+          horizontalThreshold: newProps.options?.horizontalThreshold,
+          verticalThreshold: newProps.options?.verticalThreshold,
+          crop: newProps.options?.crop,
+          useMouse: props.options?.useMouse,
+          useScreen: props.options?.useScreen,
         })
       }
     }, { deep: true })
 
     return () => (
       <div ref={viewerRef} class="vuedepthviewer__container">
-        <img src={props.img} class="vuedepthviewer__img" style={props.options.crop ? 'object-fit: cover;' : 'object-fit: contain;'} />
+        <img src={props.img} class="vuedepthviewer__img" style={props.options?.crop ? 'object-fit: cover;' : 'object-fit: contain;'} />
       </div>
     )
   }
