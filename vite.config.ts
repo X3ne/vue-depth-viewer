02882902ea86291978000e65410f72888161bda8
X3ne <X3ne@users.noreply.github.com>
2024-01-24 01:01:20 +0100

    feat: add css file to bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import banner from 'vite-plugin-banner'
-import glsl from 'vite-plugin-glsl'
-import dts from 'vite-plugin-dts'
-import pkg from './package.json'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueJsx from "@vitejs/plugin-vue-jsx"
+import banner from "vite-plugin-banner"
+import glsl from "vite-plugin-glsl"
+import dts from "vite-plugin-dts"
+import pkg from "./package.json"
+import { copyFileSync } from "fs"
 
-const outDir = 'lib'
+const outDir = "lib"
 
 export default defineConfig({
   build: {
     outDir,
     lib: {
-      entry: 'src/index.tsx',
-      name: 'vueDepthViewer',
-      formats: ['es', 'cjs', 'umd'],
+      entry: "src/index.tsx",
+      name: "vueDepthViewer",
+      formats: ["es", "cjs", "umd"],
       fileName: (format) => {
         switch (format) {
-          case 'es':
-            return 'index.mjs'
-          case 'cjs':
-            return 'index.cjs'
+          case "es":
+            return "index.mjs"
+          case "cjs":
+            return "index.cjs"
           default:
-            return 'index.min.js'
+            return "index.min.js"
         }
       },
     },
@@ -31,11 +32,21 @@ export default defineConfig({
     rollupOptions: {
       external: Object.keys(pkg.peerDependencies),
       output: {
-        exports: 'named',
+        exports: "named",
         globals: {
-          vue: 'Vue',
+          vue: "Vue",
         },
       },
+      plugins: [
+        {
+          name: "copy-css-plugin",
+          writeBundle(options, bundle) {
+            const src = "src/style.css"
+            const dest = "lib/style.css"
+            copyFileSync(src, dest)
+          },
+        },
+      ],
     },
   },
   resolve: {
@@ -52,15 +63,15 @@ export default defineConfig({
     banner({
       outDir,
       content: [
-        '/**',
+        "/**",
         ` * name: ${pkg.name}`,
         ` * version: v${pkg.version}`,
         ` * description: ${pkg.description}`,
         ` * author: ${pkg.author}`,
         ` * homepage: ${pkg.homepage}`,
         ` * license: ${pkg.license}`,
-        ' */',
-      ].join('\n'),
+        " */",
+      ].join("\n"),
     }),
   ],
 })
